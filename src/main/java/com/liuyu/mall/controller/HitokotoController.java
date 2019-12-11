997aba28c2347a01b5c01f697b7bc756c770c8f3
liuyu <30967086@qq.com>
2019-12-11 19:10:16 +0800

    整合Redis，并向Redis插入以及查询数据，两种方式，一种是在Service的实现类中添加注解，第二种就是注入RedisTemplate，使用RedisTemplate的方法进行查询以及查询值   --2019.12.11

--- src/main/java/com/liuyu/mall/controller/HitokotoController.java
+++ src/main/java/com/liuyu/mall/controller/HitokotoController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,11 +27,29 @@ public class HitokotoController{
     @Resource
     Hitokoto hitokoto;
 
+    @Resource
+    RedisTemplate<String, Object> redis;
+
     @RequestMapping(value = "/getHitokotoById", method = RequestMethod.POST)
     @ApiOperation(value = "根据ID获取一言", notes = "ID为必定提交的值，不为空")
     @ApiImplicitParam(paramType = "query", name = "Id", value = "一言id", required = true, dataType = "string")
     public String getHitokotoById(@RequestParam String Id){
         hitokoto = hitokotoService.getData(Id);
+        Object object = JSONArray.toJSON(hitokoto);
+        String json = object.toString();
+        System.out.println(json);
+        return json;
+    }
+
+    @RequestMapping(value = "/getHitokotoByRedis", method = RequestMethod.POST)
+    @ApiOperation(value = "根据ID获取一言", notes = "ID为必定提交的值，不为空")
+    @ApiImplicitParam(paramType = "query", name = "Id", value = "一言id", required = true, dataType = "string")
+    public String getHitokotoByRedis(@RequestParam String Id){
+        hitokoto = (Hitokoto) redis.opsForValue().get(Id);
+        if(hitokoto == null){
+            hitokoto = hitokotoService.getData(Id);
+            redis.opsForValue().set(hitokoto.getId(),hitokoto);
+        }
         Object object = JSONArray.toJSON(hitokoto);
         String json = object.toString();
         System.out.println(json);
--- src/main/java/com/liuyu/mall/service/impl/HitokotoServiceImpl.java
+++ src/main/java/com/liuyu/mall/service/impl/HitokotoServiceImpl.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.liuyu.mall.domain.Hitokoto;
 import com.liuyu.mall.repository.HitokotoDao;
 import com.liuyu.mall.service.HitokotoService;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -16,9 +17,10 @@ public class HitokotoServiceImpl extends ServiceImpl<HitokotoDao, Hitokoto> impl
     HitokotoDao hitokotoDao;
 
     @Override
+    @Cacheable(key = "#id", value = "Hitokoto")
     public Hitokoto getData(String id) {
         /**
-         * 021BCD54B59045F3AED360D5091FB7C3
+         *
          * 076BA86AB3C24B30BD5804C936EE3C58
          * 09383AC60A66453DAA921083B3249E4F
          * 0AFE7F2C1DD847DCBDBC3A405A1B0F92
@@ -26,7 +28,7 @@ public class HitokotoServiceImpl extends ServiceImpl<HitokotoDao, Hitokoto> impl
          * 也可以使用这个方法取值
          *      return hitokotoDao.findAllDate(id)
          * */
-        id = "076BA86AB3C24B30BD5804C936EE3C58";
+//        id = "076BA86AB3C24B30BD5804C936EE3C58";
 //        QueryWrapper<Hitokoto> queryWrapper = new QueryWrapper<>();
 //        queryWrapper.eq("id",id);
 //        Hitokoto hitokoto = this.getOne(queryWrapper);
