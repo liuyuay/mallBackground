064b487a00fb1b2da3bc79792f264b9f5723aac7
liuyu <30967086@qq.com>
2020-01-02 18:56:48 +0800

    优化。。。   --2020.01.02

--- src/main/java/com/liuyu/mall/MallApplication.java
+++ src/main/java/com/liuyu/mall/MallApplication.java
@@ -3,10 +3,14 @@ package com.liuyu.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @author liuyu
+ * @ MapperScan("com.liuyu.mall.repository") 注解扫描mapper，这里添加了，就不用在dao中写@Mapper注解了
+ * 可以添加多个扫描路径，也可以用*代替包以扫描不同包下面的DAO
+ */
 @SpringBootApplication
-@MapperScan("com.liuyu.mall.repository")        //注解扫描mapper，这里添加了，就不用在dao中写@Mapper注解了
+@MapperScan("com.liuyu.mall.repository")
 public class MallApplication {
 
     public static void main (String[] args) {
